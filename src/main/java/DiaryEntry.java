6324425cec0cea3401149b3938fc448332feffc2
LouiseSky <louise.skyborn@outlook.com>
2022-09-30 13:32:43 +0200

    First commit

---
+++ src/main/java/DiaryEntry.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+public class DiaryEntry {
+        private String title;
+        private String entry;
+        private Date date;
+
+        Date dateToday = new Date();
+    }
